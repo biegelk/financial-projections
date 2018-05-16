9031328731d481ed6ced763b52d2ed2d3ec480db
biegelk <biegelk3@gmail.com>
2018-05-16 14:23:19 -0500

    added build module

---
+++ build.py
@@ -0,0 +1,12 @@
+import math as m
+import numpy as np
+
+def build_plant(total_cost, duration):
+    inc_spend = np.zeros(duration)
+    cum_spend = np.zeros(duration)
+    for i in range(duration):
+        inc_spend[i] = total_cost / duration
+        cum_spend[i:] += total_cost / duration
+    return (inc_spend, cum_spend)
+
+def npv(total_cost, duration, mcd, term)
