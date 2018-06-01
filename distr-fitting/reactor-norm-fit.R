b119678ee7472fe4a61309783a8e635ef0933367
biegelk <biegelk3@gmail.com>
2018-06-01 14:56:55 -0500

    norm fit for schedule escalation parameters

--- distr-fitting/reactor-norm-fit.R
+++ distr-fitting/reactor-norm-fit.R
@@ -5,27 +5,27 @@
 reactorData = read.csv("~/financial-projections/data/reactors.csv")
 
 y1 <- reactorData$sesc025[0:75]
-#y1 <- subset(y1, y1 > 0)
+y1 <- y1[!is.na(y1)]
 dist1 <- fitdist(y1, "norm", start=NULL)
 plot(dist1)
 
 y2 <- reactorData$sescor2550[0:75]
-#y2 <- subset(y2, y2 > 0)
+y2 <- y2[!is.na(y2)]
 dist2 <- fitdist(y2, "norm", start=NULL)
 plot(dist2)
 
 y3 <- reactorData$sescor5075[0:75]
-#y3 <- subset(y3, y3 > 0)
+y3 <- y3[!is.na(y3)]
 dist3 <- fitdist(y3, "norm", start=NULL)
 plot(dist3)
 
 y4 <- reactorData$sescor7590[0:75]
-#y4 <- subset(y4, y4 > 0)
+y4 <- y4[!is.na(y4)]
 dist4 <- fitdist(y4, "norm", start=NULL)
 plot(dist4)
 
 y5 <- reactorData$sescor90100[0:75]
-#y5 <- subset(y5, y5 > 0)
+y5 <- y5[!is.na(y5)]
 dist5 <- fitdist(y5, "norm", start=NULL)
 plot(dist5)
 # 
